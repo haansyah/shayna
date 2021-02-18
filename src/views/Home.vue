06281feb9588a7410630b3ee63984f9de3d9fa6b
Ari Sutarman <61754538+haansyah@users.noreply.github.com>
2021-02-18 21:13:29 +0700

    materi 3 convert ke vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomanShayna />
+    <InstaShayna />
+    <PartnersShayna />
+    <FooterShayna />
+  </div>
+
+</template>
+
+<script>
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
+import WomanShayna from '@/components/WomanShayna.vue';
+import InstaShayna from '@/components/InstaShayna.vue';
+import PartnersShayna from '@/components/PartnersShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomanShayna,
+    InstaShayna,
+    PartnersShayna,
+    FooterShayna
+  }
+}
+</script>
