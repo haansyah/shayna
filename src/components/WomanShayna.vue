5b9a147bdce08657326831715102d026640e2c82
Ari Kun <61754538+haansyah@users.noreply.github.com>
2021-04-05 11:00:12 +0700

    Integrasi API Product Halaman Details.vue

--- src/components/WomanShayna.vue
+++ src/components/WomanShayna.vue
@@ -14,7 +14,7 @@
                                         <li class="w-icon active">
                                             <a href="#"><i class="icon_bag_alt"></i></a>
                                         </li>
-                                        <li class="quick-view"><router-link to="/product">+ Quick View</router-link></li>
+                                        <li class="quick-view"><router-link v-bind:to="'/product/'+itemProduct.id">+ Quick View</router-link></li>
                                     </ul>
                                 </div>
                                 <div class="pi-text">
--- src/views/Product.vue
+++ src/views/Product.vue
@@ -26,22 +26,16 @@
                             <div class="product-pic-zoom">
                                 <img class="product-big-img" :src="gambar_default" alt="" />
                             </div>
-                            <div class="product-thumbs">
-                                <carousel class="product-thumbs-track ps-slider" :dots="false" :nav="false" >
-                                    <div class="pt" @click="changeImage(thumbs[0])" :class="thumbs[0] == gambar_default ? 'active' : '' ">
-                                        <img src="img/mickey1.jpg" alt="" />
-                                    </div>
-
-                                    <div class="pt" @click="changeImage(thumbs[1])" :class="thumbs[1] == gambar_default ? 'active' : '' ">
-                                        <img src="img/mickey2.jpg" alt="" />
-                                    </div>
-
-                                    <div class="pt" @click="changeImage(thumbs[2])" :class="thumbs[2] == gambar_default ? 'active' : '' ">
-                                        <img src="img/mickey3.jpg" alt="" />
-                                    </div>
-
-                                    <div class="pt" @click="changeImage(thumbs[3])" :class="thumbs[3] == gambar_default ? 'active' : '' ">
-                                        <img src="img/mickey4.jpg" alt="" />
+                            <div class="product-thumbs" v-if="productDetails.galleries.length > 0">
+                                <carousel :dots="false" :nav="false" class="product-thumbs-track ps-slider">
+                                    <div
+                                    v-for="ss in productDetails.galleries"
+                                    :key="ss.id"
+                                    class="pt"
+                                    @click="changeImage(ss.photo)"
+                                    :class="ss.photo == gambar_default ? 'active' : '' "
+                                    >
+                                    <img :src="ss.photo" alt />
                                     </div>
                                 </carousel>
                             </div>
@@ -49,20 +43,14 @@
                         <div class="col-lg-6">
                             <div class="product-details text-left">
                                 <div class="pd-title">
-                                    <span>oranges</span>
-                                    <h3>Pure Pineapple</h3>
+                                    <span>{{ productDetails.type }}</span>
+                                    <h3>{{ productDetails.name }}</h3>
                                 </div>
                                 <div class="pd-desc">
                                     <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.
+                                        {{ productDetails.description }}
                                     </p>
-                                    <p>
-                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
-                                        Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
-                                        Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
-                                        impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
-                                    </p>
-                                    <h4>$495.00</h4>
+                                    <h4>${{ productDetails.price }}</h4>
                                 </div>
                                 <div class="quantity">
                                     <router-link to="/cart"><a href="shopping-cart.html" class="primary-btn pd-cart">Add To Cart</a></router-link>
@@ -88,6 +76,8 @@ import RelatedShayna from '@/components/RelatedShayna.vue';
 
 import carousel from 'vue-owl-carousel';
 
+import axios from "axios";
+
 
 export default {
   name: 'Product',
@@ -99,20 +89,38 @@ export default {
   },
   data() {
       return{
-          gambar_default: "img/mickey1.jpg",
+          gambar_default: '',
           thumbs: [
               "img/mickey1.jpg",
               "img/mickey2.jpg",
               "img/mickey3.jpg",
               "img/mickey4.jpg"
-          ]
+          ],
+          productDetails: []
       }
   },
   methods: {
       changeImage(urlImage) {
-          this.gambar_default = urlImage
-      }
-  }
+          this.gambar_default = urlImage;
+      },
+    setDataPicture(data) {
+        // replace object productDetails dengan data dari API
+        this.productDetails = data;
+        // replace value gambar default dengan data dari API (galleries)
+        this.gambar_default = data.galleries[0].photo;
+    },
+  },
+  mounted() {
+        axios
+            .get("http://127.0.0.1:8000/api/products", {
+                params: {
+                    id: this.$route.params.id
+                }
+            })
+            .then(res => (this.setDataPicture(res.data.data)))
+            // eslint-disable-next-line no-console
+            .catch(err => console.log(err));
+    }
 }
 </script>
 
